81d43c49d63481b82c0686b91f2828c7f4990cae
Lyricraft <yzi1b@outlook.com>
2026-04-26 23:48:58 +0800

    完善 curseforgeContentReference 并实现 curseforgeVersionReference

--- src/core/configs/prebuild/resourceItems/platformResourceItem/contentReference/curseforgeContentReference.js
+++ src/core/configs/prebuild/resourceItems/platformResourceItem/contentReference/curseforgeContentReference.js
@@ -2,6 +2,8 @@ import {ContentReference} from "./contentReference.js";
 import {stringUsable} from "../../../../../public/type.js";
 import {ArgTypeError} from "../../../../../public/errors.js";
 import {curseforgeContentGateway} from "../../../../../platforms/curseforge/curseforgeId.js";
+import {ConfigError} from "../../../../errors.js";
+import {t} from "../../../../../i18n/translate.js";
 
 const contentReferenceMap = new Map();
 
@@ -46,8 +48,34 @@ class CurseforgeSlugContentReference extends ContentReference {
 
 const idRegex = /^(0|[1-9]\d*)$/;
 const slugRegex = /^[a-z][a-z\d-]*$/i;
-const urlRegex = /curseforge\.com\/minecraft\/(mc-mods|texture-packs|shaders)\/([a-z0-9\-]+)/i;
 
+export const CURSEFORGE_CONTENT_URL_REGEX = /curseforge\.com\/minecraft\/(mc-mods|texture-packs|shaders)\/([a-z0-9\-]+)/i;
+
+export function curseforgeContentUrlToReference(url, type = null) {
+    const match = url.match(CURSEFORGE_CONTENT_URL_REGEX);
+
+    let urlType = curseforgeContentGateway.getKey(match[1].toLowerCase());
+    if (curseforgeContentGateway.hasValue(urlType)) {
+        urlType = curseforgeContentGateway.getKey(urlType);
+    } else {
+        return null;
+    }
+
+    const idOrSlug = match[2];
+
+    if (stringUsable(type) && urlType !== type) {
+        throw new ConfigError(t('error.configs.platformResourceReferenceTypeNotMatch',
+            idOrSlug, urlType, type));
+    }
+
+    if (idRegex.test(idOrSlug)) {
+        return new CurseforgeIdContentReference(idOrSlug, urlType);
+    } else if (slugRegex.test(idOrSlug)) {
+        return new CurseforgeSlugContentReference(idOrSlug, urlType);
+    } else {
+        return null;
+    }
+}
 
 contentReferenceMap
     .set(idRegex, CurseforgeIdContentReference)
@@ -57,15 +85,4 @@ contentReferenceMap
         }
         return new CurseforgeSlugContentReference(slug.toLowerCase(), type);
     })
-    .set(urlRegex, function (str, type0) {
-        const match =  str.match(urlRegex);
-        const type = curseforgeContentGateway.getKey(match[1].toLowerCase());
-        const idOrSlug = match[2].toLowerCase();
-        if (idRegex.test(idOrSlug)) {
-            return new CurseforgeIdContentReference(idOrSlug, type);
-        } else if (slugRegex.test(idOrSlug)) {
-            return new CurseforgeSlugContentReference(idOrSlug, type);
-        } else {
-            return null;
-        }
-    });
+    .set(CURSEFORGE_CONTENT_URL_REGEX, curseforgeContentUrlToReference());
---
+++ src/core/configs/prebuild/resourceItems/platformResourceItem/versionReference/curseforgeVersionReference.js
@@ -0,0 +1,50 @@
+import {VersionReference} from "./versionReference.js";
+import {ConfigError} from "../../../../errors.js";
+import {t} from "../../../../../i18n/translate.js";
+import {
+    CURSEFORGE_CONTENT_URL_REGEX,
+    curseforgeContentUrlToReference
+} from "../contentReference/curseforgeContentReference.js";
+
+const versionReferenceMap = new Map();
+
+export function curseforgeVersionReference(str, contentReference = null) {
+    for (const [key, value] of versionReferenceMap) {
+        if (key.test(str)) {
+            return value(str, contentReference);
+        }
+    }
+    return null;
+}
+
+class CurseforgeIdVersionReference extends VersionReference {
+    constructor(id, contentReference = null) {
+        super(contentReference);
+        this.id = id;
+    }
+}
+
+export const CURSEFORGE_DOWNLOAD_URL_REGEX = /media\.forgecdn\.net\/files\/(\d{3})(\d+)\/([^/]+)$/;
+
+versionReferenceMap
+    .set(/^(0|[1-9]\d*)$/, CurseforgeIdVersionReference)
+    .set(CURSEFORGE_CONTENT_URL_REGEX, function (url, contentReference = null) {
+        // 检查是不是 version url，并获取 version 信息。
+        const match = url.match(/\/(files|download)\/(0|[1-9]\d*)/);
+        if (!match) {
+            return null;
+        }
+        const fileId = match[2];
+
+        const urlContentReference = curseforgeContentUrlToReference(url);
+        if (contentReference && !urlContentReference.checkMatch(contentReference)) {
+            throw new ConfigError(t('error.configs.platformResourceReferenceContentNotMatch',
+                url, urlContentReference.symbolType(), urlContentReference.symbol(), contentReference.symbol()));
+        }
+        return new CurseforgeIdVersionReference(fileId, contentReference??urlContentReference);
+    })
+    .set(CURSEFORGE_DOWNLOAD_URL_REGEX, function (url, contentReference = null) {
+        const match = url.match(CURSEFORGE_DOWNLOAD_URL_REGEX)
+        const id = match[1] + match[2];
+        return new CurseforgeIdVersionReference(id, contentReference);
+    });
